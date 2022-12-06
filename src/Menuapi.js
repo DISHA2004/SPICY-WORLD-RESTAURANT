016452c35a738fec3fbec723e5ee87a7ee10021f
DISHA2004 <78337260+DISHA2004@users.noreply.github.com>
2022-12-06 19:44:19 +0530

    committed react project

---
+++ src/Menuapi.js
@@ -0,0 +1,114 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/dhokla.jpg",
+      name: "DHOKLA",
+      category: "INDIAN",
+      price: "42₹",
+      description:
+        "I love DHOKLA realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/aaloo.jpg",
+      name: "ALOO SBJI",
+      category: "INDIAN",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/dosa.jpg",
+      name: "DOSA",
+      category: "INDIAN",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "/images/paneer.jpg",
+      name: "PANEER",
+      category: "INDIAN",
+      price: "90₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "images/pizza.jpg",
+      name: "PIZZA",
+      category: "CHINESE",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/noodles.jpg",
+      name: "NOODLES",
+      category: "CHINESE",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/momo.jpg",
+      name: "MOMOS",
+      category: "CHINESE",
+      price: "160₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rice.jpg",
+      name: "RICE",
+      category: "INDIAN",
+      price: "90₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/poha.jpg",
+      name: "POHA",
+      category: "INDIAN",
+      price: "110₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 10,
+        image: "../images/springroll.jpg",
+        name: "SPRING ROLL",
+        category: "CHINESE",
+        price: "70₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+      {
+        id: 11,
+        image: "../images/utpam.jpg",
+        name: "UTPAM",
+        category: "INDIAN",
+        price: "160₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+      {
+        id: 12,
+        image: "../images/burger.jpg",
+        name: "BURGER",
+        category: "CHINESE",
+        price: "100₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+  ];
+  
+  export default Menu;
+  
